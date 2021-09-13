67347461cb01d1b9c3389973c03cee792b3e44b1
Martin Leimer <martin.leimer@zuehlke.com>
2021-09-13 12:12:23 +0200

    feat: add 404 and 410 pages

--- src/routes/pizzas/[id].svelte
+++ src/routes/pizzas/[id].svelte
@@ -2,6 +2,19 @@
     import type {LoadInput, LoadOutput} from '@sveltejs/kit';
 
     export function load({page}: LoadInput<{ id: string }>): LoadOutput<{ id: string }> {
+        if (page.params.id === '404') {
+            return {
+                status: 404,
+                error: new Error('This pizza does not exists')
+            };
+        }
+        if (page.params.id === '410') {
+            return {
+                status: 410,
+                error: new Error('We no longer serve this pizza')
+            };
+        }
+
         return {
             props: {
                 id: page.params.id,
