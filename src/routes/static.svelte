3c6470cc96e4750cac9b7979083fd11d92c69a75
Martin Leimer <martin.leimer@zuehlke.com>
2021-09-13 11:42:50 +0200

    feat: add static pre-rendered page

---
+++ src/routes/static.svelte
@@ -0,0 +1,22 @@
+<script context="module" lang="ts">
+    import type {LoadOutput} from '@sveltejs/kit';
+
+    export const prerender = true;
+    export function load(): LoadOutput<{ preRenderedRandomNumber: number }> {
+        return {
+            props: {
+                preRenderedRandomNumber: Math.random(),
+            }
+        };
+    }
+</script>
+
+<script lang="ts">
+    export let preRenderedRandomNumber: number;
+</script>
+
+<svelte:head>
+    <title>Static page</title>
+</svelte:head>
+
+<h1>Pre-Rendered page with static random number: {preRenderedRandomNumber}</h1>
