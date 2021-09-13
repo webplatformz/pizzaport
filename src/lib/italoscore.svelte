d21b55d0c0d9d9ad492057a8d8880ec4828e1584
Ben Millo <bmillo@gmail.com>
2021-09-13 11:41:59 +0200

    Added draft for italo score component

---
+++ src/lib/italoscore.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    let italoRange = [
+        {score_min_percent: 90, title: 'Super Mario', iconUrl: 'mario.png'},
+        {score_min_percent: 80, title: 'Luigi', iconUrl: 'mario.png'},
+        {score_min_percent: 0, title: 'Bowser', iconUrl: 'mario.png'},
+    ];
+
+    export let scorePercent: number = 0;
+    $: myscore = getItaloScoreItem(scorePercent);
+
+    function getItaloScoreItem(value) {
+        return italoRange.find(function (element) {
+            return value >= element.score_min_percent;
+        });
+    }
+</script>
+
+<div>
+    <p>{myscore.title}</p>
+    <img src={myscore.iconUrl} alt="How italian are you">
+    <p>
+        <progress value={scorePercent/100}></progress> {scorePercent}%
+    </p>
+</div>
+
+<style>
+    p {
+        color: darkgreen;
+    }
+
+    img {
+        width: 30px;
+    }
+</style>
+
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-	export let name = 'Pizza';
+	export let name = 'PizzaPort';
+    import ItaloScore from '../lib/italoscore.svelte';
 </script>
 
 <p>Hello {name}</p>
+<ItaloScore scorePercent="80"/>
