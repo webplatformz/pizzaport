532000dce61abcce34ed9f2b4964dc5dd5ac8c8a
Martin Leimer <martin.leimer@zuehlke.com>
2021-09-13 17:15:27 +0200

    feat: link pizza details with list

--- src/routes/pizzas/index.svelte
+++ src/routes/pizzas/index.svelte
@@ -1,5 +1,41 @@
+<script context="module" lang="ts">
+    import type {LoadInput, LoadOutput} from '@sveltejs/kit';
+
+    export async function load({fetch}: LoadInput): Promise<LoadOutput<{ items: { name: string, url: string }[] }>> {
+        const url = `/pizzas.json`;
+        const res = await fetch(url);
+        const {pizzas} = await res.json();
+
+        if (res.ok) {
+            return {
+                props: {
+                    items: pizzas
+                }
+            };
+        }
+
+        return {
+            status: 500,
+            error: new Error('Internal server error')
+        };
+    }
+</script>
+
+<script lang="ts">
+    import NavLink from '../../lib/NavLink.svelte';
+
+    export let items: { name: string, url: string }[];
+</script>
+
 <svelte:head>
-	<title>Pizza List</title>
+    <title>Pizza List</title>
 </svelte:head>
 
 <h1>Pizza List</h1>
+<ul>
+    {#each items as {name, url}}
+        <li>
+            <NavLink path={url}>{name}</NavLink>
+        </li>
+    {/each}
+</ul>
