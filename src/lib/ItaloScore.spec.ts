3f99c001df3407ec9ec329ca937230b39151bbc0
Lucas Schnüriger <lucas.schnueriger@me.com>
2021-09-15 09:17:52 +0200

    Move ItaloScore.spec.ts to lib

--- src/lib/ItaloScore.spec.ts
+++ src/lib/ItaloScore.spec.ts
@@ -1,9 +1,9 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/svelte';
-import App from '../../src/lib/ItaloScore.svelte';
+import ItaloScore from './ItaloScore.svelte';
 
 it('should show italo score with mario as result', () => {
-	const { getByText } = render(App, { scorePercent: 95 });
+	const { getByText } = render(ItaloScore, { scorePercent: 95 });
 
 	expect(getByText('Super Mario')).toBeVisible();
 	expect(getByText('95%')).toBeVisible();
