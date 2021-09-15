28037a5b67f07c5493625d3f848ac403096d9479
Martin Leimer <martin.leimer@zuehlke.com>
2021-09-15 11:56:46 +0200

    test: adjust tests to renamed props

--- src/lib/PizzaSelection/PizzaSelection.spec.ts
+++ src/lib/PizzaSelection/PizzaSelection.spec.ts
@@ -1,28 +1,29 @@
 import { fireEvent, render } from '@testing-library/svelte';
 import PizzaSelection from './PizzaSelection.svelte';
+import type {PizzaListItem} from '../../routes/pizzas/index.json';
 
 it('should add pizza to selection', async () => {
-	const pizzas = ['a', 'b', 'c'];
-	const selectedPizzas: string[] = [];
-	const { getByText, component } = render(PizzaSelection, { pizzas, selectedPizzas });
+	const pizzas: PizzaListItem[] = ['a', 'b', 'c'].map(n => ({name: n, url: n, imgSrc: n}));
+	const selectedPizzaNames: string[] = [];
+	const { getByText, component } = render(PizzaSelection, { pizzas, selectedPizzaNames });
 
 	await fireEvent.click(getByText('a'));
 
-	expect(component.selectedPizzas).toEqual(['a']);
+	expect(component.selectedPizzaNames).toEqual(['a']);
 });
 
 it('should remove pizza from selection', async () => {
-	const pizzas = ['a', 'b', 'c'];
-	const selectedPizzas = ['a'];
-	const { getByText, component } = render(PizzaSelection, { pizzas, selectedPizzas });
+	const pizzas: PizzaListItem[] = ['a', 'b', 'c'].map(n => ({name: n, url: n, imgSrc: n}));
+	const selectedPizzaNames = ['a'];
+	const { getByText, component } = render(PizzaSelection, { pizzas, selectedPizzaNames });
 
 	await fireEvent.click(getByText('a'));
 
-	expect(component.selectedPizzas).toEqual([]);
+	expect(component.selectedPizzaNames).toEqual([]);
 });
 
 it('should have all pizzas displayed', () => {
-	const pizzas = ['a', 'b', 'c'];
+	const pizzas: PizzaListItem[] = ['a', 'b', 'c'].map(n => ({name: n, url: n, imgSrc: n}));
 
 	const { container } = render(PizzaSelection, { pizzas });
 
