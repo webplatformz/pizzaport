c8413d8369a4fae505411b52880edb036d603b83
Lucas Schnüriger <lucas.schnueriger@me.com>
2021-09-15 17:54:24 +0200

    Add self-made translations functionality

---
+++ locales/en.ts
@@ -0,0 +1,16 @@
+export default {
+	dashboard: 'Dashboard',
+	pizzas: 'Pizzas',
+	subscription: 'Subscription',
+	statistics: 'Statistics',
+	'service': 'Service',
+	'service.configure_pizza': 'Configure your pizza subscription',
+	'service.size': 'Size',
+	'service.amount': 'Amount',
+	'service.frequency': 'Frequency',
+	'service.delivery_time': 'Delivery Time',
+	'frequency.start': 'Start',
+	'frequency.end': 'End',
+	'frequency.daily': 'Daily',
+	'frequency.weekly': 'Weekly',
+};
